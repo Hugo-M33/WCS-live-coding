75fca41c21ba81b56bd55604fb6d0eb4bc6b84d5
Hugo Martin <hugo.martin.duloz@gmail.com>
2022-09-16 15:03:43 +0200

    :sparkles: Add Avatars

--- client/src/components/Wilder.js
+++ client/src/components/Wilder.js
@@ -3,16 +3,29 @@ import SkillPicker from "./SkillPicker"
 import CrossButton from "./CrossButton";
 import useWilder from "../hooks/useWilder";
 import blank_picture from "../assets/black_picture.png"
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 
 const Wilder = ({wilder}) => {
     const {id, name, skills, description} = wilder;
-    console.log(wilder)
+    const [picture, setPicture] = useState(blank_picture);
+    useEffect(() => {
+        const getPic = async () => {
+            const p = await axios.get(`https://api.multiavatar.com/${name}.png/?apikey=zASjtU4lQcnwNu`, {
+                responseType: 'blob'
+            })
+                .then(response => URL.createObjectURL(response.data))
+            console.log(p)
+            setPicture(p)
+        }
+        getPic()
+    }, [])
     const {deleteWilder} = useWilder()
     return (
         <article className="card">
             <CrossButton className={"delete-wilder-btn"} onClick={() => deleteWilder(id)}>X</CrossButton>
-            <img src={blank_picture} alt="Jane Doe Profile"/>
+            <img src={picture} alt="Jane Doe Profile"/>
             <h3>{name}</h3>
             <p>
                 {description || <i>No description...</i>}
--- client/src/services/APIClient.js
+++ client/src/services/APIClient.js
@@ -4,5 +4,5 @@ export default axios.create({
     baseURL: 'http://localhost:3000/api/',
     headers: {
         'Content-Type': 'application/json'
-    }
+    },
 })
